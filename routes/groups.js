213529bbe9cb3b25745c128b71b11adfadec1a1e
nclongkk <nclongkk@gmail.com>
2021-10-01 17:07:02 +0700

    change protect type

--- routes/groups.js
+++ routes/groups.js
@@ -11,14 +11,9 @@ const {
   updateWorkDay,
 } = require("../controllers/groups");
 
-const { protect } = require("../middleware/auth");
-
-router.route("/").get(protect, getGroups).post(protect, createGroup);
-router.route("/:id").patch(protect, updateGroup).delete(protect, deleteGroup);
-router.route("/:id/member").put(protect, addMember);
-router
-  .route("/:id/member/:memberId")
-  .patch(protect, updateWorkDay)
-  .delete(protect, deleteMember);
+router.route("/").get(getGroups).post(createGroup);
+router.route("/:id").patch(updateGroup).delete(deleteGroup);
+router.route("/:id/member").put(addMember);
+router.route("/:id/member/:memberId").patch(updateWorkDay).delete(deleteMember);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const { updateUser, personalSchedule } = require("../controllers/users");
 
-const { protect } = require("../middleware/auth");
-
-router.route("/").patch(protect, updateUser);
-router.route("/schedule").get(protect, personalSchedule);
+router.route("/").patch(updateUser);
+router.route("/schedule").get(personalSchedule);
 
 module.exports = router;
